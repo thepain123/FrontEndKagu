6cad2e9870ac6d302b934c62675e18d77c9ef2a3
thepain123 <vuminhduc2598@gmail.com>
2020-06-04 12:01:16 +0700

    fix routing

--- src/app/entities/entities.module.ts
+++ src/app/entities/entities.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from "@angular/common";
 import { EntitiesRoutingModule } from "./entities-routing.module";
 import { EntitiesComponent } from "./entities.component";
 import { FormsModule } from "@angular/forms";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 @NgModule({
-  declarations: [EntitiesComponent],
-  exports: [EntitiesComponent],
+  declarations: [EntitiesComponent, ProductDetailsComponent],
+  exports: [EntitiesComponent, ProductDetailsComponent],
   imports: [CommonModule, EntitiesRoutingModule, FormsModule],
 })
 export class EntitiesModule {}
